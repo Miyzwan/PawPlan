27f3f3ceadbeae5e5e99974b9697efede1ac8891
Dimas Dwi Ismaunnizam <dimasdwiisma@gmail.com>
2026-06-25 02:40:13 +0700

    Initial commit

---
+++ PawPlan Watch App/PawPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPlanApp.swift
+//  PawPlan Watch App
+//
+//  Created by Dimas Dwi Ismaunnizam on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PawPlan_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawPlan/PawPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPlanApp.swift
+//  PawPlan
+//
+//  Created by Dimas Dwi Ismaunnizam on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PawPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
